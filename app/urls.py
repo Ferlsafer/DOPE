9213240c76da4e03967d3384367c1a4684e1129f
James Mashaka <71042708+islandkid-20@users.noreply.github.com>
2024-10-07 12:20:04 +0300

    Edit, Delete and template formating

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('add_category', views.add_category, name='add_category')
+    path('add_category', views.add_category, name='add_category'),
+    path('delete_category/<str:id>', views.delete_category, name="delete_category"),
+    path('edit_category/<str:id>', views.edit_category, name="edit_category")
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from app.models import *
 # Create your views here.
 def home(request):
     categories = Category.objects.all().order_by('-id')
-    return render(request, 'home.html', {'categories': categories})
+    return render(request, 'category/index.html', {'categories': categories})
 
 def add_category(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        Category.objects.create(name=name)
+    try:
+        if request.method == 'POST':
+            name = request.POST['name']
+            Category.objects.create(name=name)
+            messages.success(request, "Category added successfuly")
+            return redirect('home')
+    except Exception as e:
+        messages.error(request, "Error occured")
+        return redirect('home')
+
+def delete_category(request, id):
+    try:
+        category = get_object_or_404(Category, id=id)
+        category.delete()
+        messages.success(request, "Category deleted successfuly")
+        return redirect('home')
+    except Category.DoesNotExist as e:
+        print(str(e))
+        messages.error(request, "Error occured")
+        return redirect('home')
+
+def edit_category(request, id):
+    try:
+        if request.method == "POST":
+            name = request.POST['name']
+            category = get_object_or_404(Category, id=id)
+            category.name = name
+            category.save()
+            messages.success(request, "Category updated successfuly")
+            return redirect('home')
+        category = get_object_or_404(Category, id=id)
+        return render(request, 'category/edit.html', {'category': category})
+    except Category.DoesNotExist as e:
+        print(str(e))
+        messages.error(request, str(e.args[0]))
         return redirect('home')
